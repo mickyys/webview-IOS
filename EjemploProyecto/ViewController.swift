f2e40615be6394310eadfa574fbc3c05f6a2159f
Hector Martinez <halejmartinez@falabella.cl>
2023-04-19 12:05:45 -0400

    webview poc

--- EjemploProyecto/ViewController.swift
+++ EjemploProyecto/ViewController.swift
@@ -1,19 +1,27 @@
-//
-//  ViewController.swift
-//  EjemploProyecto
-//
-//  Created by Hector Martinez on 17-04-23.
-//
-
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        webView = WKWebView(frame: view.bounds)
+        webView.navigationDelegate = self
+        view.addSubview(webView)
+        
+        if let url = URL(string: "https://payments-psp-int.fpayapp.com/checkout-fpay-v2/cl/payment/643dadd1f5604655a5f68367") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
-
-
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            // Obtener el agente de usuario actual
+            let userAgent = webView.value(forKey: "userAgent") as? String
+            
+            // Imprimir el agente de usuario para depuración
+            print("User Agent: \(userAgent ?? "")")
+   }
 }
-
